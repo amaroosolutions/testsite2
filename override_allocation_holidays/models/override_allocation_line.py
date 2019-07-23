db8498e87012e37454513612f0af955e6d4fb76d
amaroosolutions <35333236+amaroosolutions@users.noreply.github.com>
2019-07-23 12:41:50 +0100

    Add files via upload

---
+++ override_allocation_holidays/models/override_allocation_line.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+#
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2018-today Ascetic Business Solution <www.asceticbs.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#################################################################################
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+## Inherit class sale.order.line for validate amount of product
+class SaleOrderLine(models.Model):
+    _inherit = "hr.leave.allocation"
+
+    @api.multi
+    @api.constrains('holiday_status_id')
+    def _check_leave_type_validity(self):
+        for allocation in self:
+            if allocation.holiday_status_id.validity_start and allocation.holiday_status_id.validity_stop:
+                vstart = allocation.holiday_status_id.validity_start
+                vstop = allocation.holiday_status_id.validity_stop
+                today = fields.Date.today()
+
+                if vstart > today or vstop < today:
+                    raise UserError(_('You  TEST can allocate %s only between %s and %s') % (allocation.holiday_status_id.display_name,
+                                                                                  allocation.holiday_status_id.validity_start, allocation.holiday_status_id.validity_stop))
+
